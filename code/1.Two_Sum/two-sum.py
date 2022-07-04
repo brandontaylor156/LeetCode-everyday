72a5c72d672d36bc3a89a48698f4f5e960940c96
brandontaylor156 <brandon.david4@outlook.com>
2022-07-03 20:28:31 -0700

    two sum

---
+++ code/1.Two_Sum/two-sum.py
@@ -0,0 +1,7 @@
+# Source : https://leetcode.com/problems/two-sum/
+# Author : Brandon Taylor
+# Date   : 2022-07-03
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
