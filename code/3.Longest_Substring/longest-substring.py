df16214f83a99d88541edbb8b96ac3fa70d2c457
brandontaylor156 <brandon.david4@outlook.com>
2022-07-05 20:31:19 -0700

    not finished

--- code/3.Longest_Substring/longest-substring.py
+++ code/3.Longest_Substring/longest-substring.py
@@ -1,5 +1,16 @@
-s = "abcdefghijkladdfwertdcvxcsege"
+s = "abcdefgggabcdefghijklmnop"
 
-substring = []
-substring.push(s[0])
+startIndex = 0
+substringLength = 1
 max = 1
+
+for character in range(1, len(s)):
+    substringLength += 1
+    for i in range(startIndex, character+1):
+        if (s[i] == s[character] and i != character):
+            if substringLength > max:
+                max = substringLength
+            substringLength = 1
+            startIndex += 1
+
+print(max)
