9ce457ed5a81470061baeb1b00e281cabebc4796
brandontaylor156 <brandon.david4@outlook.com>
2022-07-10 18:23:12 -0700

    solved

--- code/9.Palindrome_Number/palindrome-number.py
+++ code/9.Palindrome_Number/palindrome-number.py
@@ -1,12 +1,23 @@
-def isPalindrome(self, x: int) -> bool:
+def isPalindrome(x):
     listNums = []
     output = False
-    while x % 10 != 0:
+
+    if x < 0:
+        return output
+    elif x >= 0 and x < 10:
+        output = True
+        return output
+
+    while x / 10 != 0:
         listNums.append(x % 10)
         x = int(x / 10)
+        
     for num in range(int(len(listNums)/2)):
         if listNums[num] == listNums[len(listNums)-1-num]:
             output = True
         else:
             output = False
+            return output
     return output
+
+print(isPalindrome(1000021))
