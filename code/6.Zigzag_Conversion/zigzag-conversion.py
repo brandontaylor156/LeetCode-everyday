737a89e6f47a6e252582fa7731519f70c7d34838
brandontaylor156 <brandon.david4@outlook.com>
2022-07-11 23:04:19 -0700

    comments

--- code/6.Zigzag_Conversion/zigzag-conversion.py
+++ code/6.Zigzag_Conversion/zigzag-conversion.py
@@ -1,19 +1,22 @@
 def convert(s, numRows):
     line = 0
-    pl = 1
+    # direction 1 goes down, direction -1 goes up at zig zag angle
+    lineDirection = 1
     output = [""] * numRows
     for i in range(len(s)):
         output[line] += s[i]
         if numRows > 1:
-            line += pl
+            line += lineDirection
+            # if line reaches last row index in given direction, switch directions
             if line == 0 or line == numRows-1:
-                pl *= -1
+                lineDirection *= -1
+    print(output)
     outputStr = ""
     for j in range(numRows):
         outputStr+=output[j]
     return outputStr
 
 s = "abcdefghijklmnop"
-numRows = 3
+numRows = 9
 print(convert(s, numRows))
 
